a05a5cf63d1e5129d06d3fb8d48703ae5741acf7
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2023-01-31 14:22:20 +1100

    remove unnecessary comment

--- src/examples/tooltip_example.tsx
+++ src/examples/tooltip_example.tsx
@@ -112,88 +112,86 @@ const StateImpl = () => {
   );
 };
 
-const tooltipMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2qA2yCWAHAdABbYQRgB2AxALIDyAqgMoCiA+swHIAqzASqwEEOAYQAStXgG0ADAF1EoXKljYcqcgpAAPRAFYAbAEZ80gCwBOU6YBMADkNXTu3QBoQAT0T38pgOznDIwNrAGZfEOlrAF8otzRMHAJYQlQAdyo6JjYAGWYBADU2ITEJGXkkECUVNQ0KnQRrMxNzfRDdX10Qo3NzX2s3TwRdaWNzWyszcfMI218YuPQsPHxktIyGFlZcgrYuWlpsrgBJAAUyzSrVbHVNesbffENba2sWkMNrfWlfU30BxDa0nwummfV8tnMzlsIXmIHiSySKXSNA2bGE2SOwgA0oIROIpHILsorjc6nojCYLFY7A4rM5-ghTCMTO1XrZhvo7NZdLD4YkVkjsOQoCisls8oVcSUCeVFMSardEI1rPhXpZoVCWuz+h4lZFVYZpOZXjZ2V0urzFvzVqkhSLMpt0ZiccV8ecKpcFWSGvq1aYNboIfptQzuSr9D9frpAtJbK1LQllja7ZQtLBkABDZBgfAZgBm2YATgAKTg8fgAEWY2QEAE0AJSUPlJwXC91y6rXWqgO6+nr+tqBrW6HWDCIhR5hYfSIyvaKxOFW5bECAph1sMt8KVuwke+VdxU+lV+gNBkO6xnM6SssYcrk8hfNggrteo9jcLd7A7HM67jsk7ttD1Y9+1PYdR0QJxjBHWMQhsEI4yNecFkTZ8SFfMUnWxbdSj-Sp91JHtgNVUDBzPEcGWcFUrH8JlA0CNpTATBEiHQ4VU3TLMc3zIti22SUqxrBsmyXNDVzbPDPQPb1lRI9UyPAhkvhVAJDGjQxDGNEI2hiBdyFQMh4AqJ8iU7QigIQABaP4L2s5j+RXMhAKk8z6meB5aNCWxnj8F5XAvblzEeawLF0KxNPBY17JbNZTIAw8nCCswbH8AxjScToGSZaDqV8fRTECfRozCaLETSO04q9IjGQghBjRMQwIkaqxOSedpStY8SoEq6TqsDfAQhpRpzGvJkqVMBlzXwDpnHCaxGuCcxdKiIA */
-  createMachine(
-    {
-      id: 'tooltip',
-      initial: 'hidden',
-      states: {
-        hidden: {
-          on: {
-            MOUSE_ENTER_ANCHOR: {
-              target: 'showing',
-            },
-          },
-        },
-        shown: {
-          on: {
-            MOUSE_LEAVE_ANCHOR: {
-              target: 'hiding',
-            },
-            MOUSE_LEAVE_TOOLTIP: {
-              target: 'hiding',
-            },
-            MOUSE_CLICK_ANCHOR: {
-              target: 'hidden',
-            },
-          },
-        },
-        showing: {
-          after: {
-            ENTER_DELAY: {
-              target: '#tooltip.shown',
-              actions: [],
-              internal: false,
-            },
-          },
-          on: {
-            MOUSE_LEAVE_ANCHOR: {
-              target: 'hidden',
-            },
-            MOUSE_CLICK_ANCHOR: {
-              target: 'hidden',
-            },
-          },
-        },
-        hiding: {
-          after: {
-            LEAVE_DELAY: {
-              target: '#tooltip.hidden',
-              actions: [],
-              internal: false,
-            },
-          },
-          on: {
-            MOUSE_ENTER_ANCHOR: {
-              target: 'shown',
-            },
-            MOUSE_ENTER_TOOLTIP: {
-              target: 'shown',
-            },
-            MOUSE_CLICK_ANCHOR: {
-              target: 'hidden',
-            },
+const tooltipMachine = createMachine(
+  {
+    id: 'tooltip',
+    initial: 'hidden',
+    states: {
+      hidden: {
+        on: {
+          MOUSE_ENTER_ANCHOR: {
+            target: 'showing',
           },
         },
       },
-      schema: {
-        context: {} as {},
-        events: {} as
-          | { type: 'MOUSE_LEAVE_ANCHOR' }
-          | { type: 'MOUSE_ENTER_ANCHOR' }
-          | { type: 'MOUSE_ENTER_TOOLTIP' }
-          | { type: 'MOUSE_LEAVE_TOOLTIP' }
-          | { type: 'MOUSE_CLICK_ANCHOR' },
+      shown: {
+        on: {
+          MOUSE_LEAVE_ANCHOR: {
+            target: 'hiding',
+          },
+          MOUSE_LEAVE_TOOLTIP: {
+            target: 'hiding',
+          },
+          MOUSE_CLICK_ANCHOR: {
+            target: 'hidden',
+          },
+        },
       },
-      context: {},
-      predictableActionArguments: true,
-      preserveActionOrder: true,
+      showing: {
+        after: {
+          ENTER_DELAY: {
+            target: '#tooltip.shown',
+            actions: [],
+            internal: false,
+          },
+        },
+        on: {
+          MOUSE_LEAVE_ANCHOR: {
+            target: 'hidden',
+          },
+          MOUSE_CLICK_ANCHOR: {
+            target: 'hidden',
+          },
+        },
+      },
+      hiding: {
+        after: {
+          LEAVE_DELAY: {
+            target: '#tooltip.hidden',
+            actions: [],
+            internal: false,
+          },
+        },
+        on: {
+          MOUSE_ENTER_ANCHOR: {
+            target: 'shown',
+          },
+          MOUSE_ENTER_TOOLTIP: {
+            target: 'shown',
+          },
+          MOUSE_CLICK_ANCHOR: {
+            target: 'hidden',
+          },
+        },
+      },
     },
-    {
-      delays: {
-        ENTER_DELAY: 500,
-        LEAVE_DELAY: 1000,
-      },
-    }
-  );
+    schema: {
+      context: {} as {},
+      events: {} as
+        | { type: 'MOUSE_LEAVE_ANCHOR' }
+        | { type: 'MOUSE_ENTER_ANCHOR' }
+        | { type: 'MOUSE_ENTER_TOOLTIP' }
+        | { type: 'MOUSE_LEAVE_TOOLTIP' }
+        | { type: 'MOUSE_CLICK_ANCHOR' },
+    },
+    context: {},
+    predictableActionArguments: true,
+    preserveActionOrder: true,
+  },
+  {
+    delays: {
+      ENTER_DELAY: 500,
+      LEAVE_DELAY: 1000,
+    },
+  }
+);
